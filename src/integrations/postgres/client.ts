a1ce9908e6da1b004960795e28e7c8e83d2ae0d1
hemypo <hemypo@yandex.ru>
2025-06-15 20:23:30 +0300

    connect

--- src/integrations/postgres/client.ts
+++ src/integrations/postgres/client.ts
@@ -3,11 +3,11 @@ import { Pool } from 'pg';
 
 // TODO: Update these values with your new PostgreSQL server credentials
 export const pool = new Pool({
-  host: "YOUR_PG_HOST",
-  port: 5432,
-  database: "YOUR_PG_DATABASE",
-  user: "YOUR_PG_USER",
-  password: "YOUR_PG_PASSWORD",
+  host: "https://analyticsdb.makeagency.ru",
+  port: 48302,
+  database: "pg_main_apps",
+  user: "apps",
+  password: "B2cS_2WvYc3kV308",
   ssl: { rejectUnauthorized: false } // Remove 'ssl' if not required
 });
 
